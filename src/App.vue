a9595f3a81252cd37a8cd1fdf5f32e7d1825996a
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-03-29 14:50:13 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto Medium",sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
