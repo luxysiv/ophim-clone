5d156c392fcb6773b81946846626dc7253c89af8
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-03-31 14:27:54 +0700

    Update index.js

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'vuex'
+import mutations from "./mutations"
+import state from "./state"
+
+const store = createStore({
+    state,
+    mutations
+  })
+export default store
