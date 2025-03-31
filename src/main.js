9c268532bae751bf1580e709b95ebd2d428eb212
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-03-31 15:33:37 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import ElementPlus from 'element-plus'
