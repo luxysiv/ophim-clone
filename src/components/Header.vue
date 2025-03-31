b3a26ee777f440357cf2c7a2a8ea9064db210cc9
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-03-31 15:22:48 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,49 +1,42 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand to="/">
-      <img src="/vieon-logo.png" alt="VieON - Xem phim trực tuyến" class="logo" />
-    </b-navbar-brand>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :active="$route.path === '/home'" to="/home">{{$t('Trang chủ')}}</b-nav-item>
-        <b-nav-item :active="$route.path === '/phim-bo'" to="/phim-bo">{{$t('Phim Bộ')}}</b-nav-item>
-        <b-nav-item :active="$route.path === '/phim-le'" to="/phim-le">{{$t('Phim Lẻ')}}</b-nav-item>
-        
-        <b-nav-item-dropdown :text="$t('Thể loại')" right>
-          <b-dropdown-item v-for="genre in genres" :key="genre.path" :to="{name: 'TheLoai', params:{path: genre.path}}">
-            {{ genre.name }}
-          </b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown :text="$t('Quốc gia')" right>
-          <b-dropdown-item v-for="country in countries" :key="country.path" :to="{name: 'QuocGia', params:{path: country.path}}">
-            {{ country.name }}
-          </b-dropdown-item>
-        </b-nav-item-dropdown>
-        
-        <b-nav-item :active="$route.path === '/phim-sap-chieu'" to="/phim-sap-chieu">{{$t('Sắp chiếu')}}</b-nav-item>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
       </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Tìm kiếm phim..."></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" @click="searchMovie">Search</b-button>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
 
-        <b-nav-item-dropdown :text="$t('Ngôn ngữ')" right>
-          <b-dropdown-item v-for="lang in languages" :key="lang.title" @click="changeLanguage(lang.title)">
-            {{ lang.name }}
-          </b-dropdown-item>
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
         </b-nav-item-dropdown>
 
-        <b-nav-item href="/profile" :title="$t('Tài khoản')">
-          <b-icon icon="person-circle"></b-icon>
-        </b-nav-item>
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
+</div>
 </template>
 
 <script>
