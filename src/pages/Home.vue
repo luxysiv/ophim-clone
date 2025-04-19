9629e038ed9576cae8a7c6697282a58bbe0ecf2b
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-04-19 23:41:10 +0700

    Upload

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,80 +1,94 @@
 <template>
-  
-      <div style="width: 100%">
-        <CarouselPage />
+  <div style="width: 100%">
+    <CarouselPage />
 
-        <!-- Danh mục phim -->
-        <div v-for="(section, sectionIndex) in sections" :key="sectionIndex" >
-          <v-row class="category-header" align="center" no-gutters>
-            <v-col cols="auto">
-              <h2 class="category-title">{{ section.title }}</h2>
-            </v-col>
-            <v-col cols="auto">
-              <router-link
-                :to="section.name === 'QuocGia' || section.name === 'PhimNew'
-                  ? { name: section.name, params: { path: section.id.split('/')[1] } }
-                  : { name: section.name }"
-                class="view-all"
-              >
-                Xem tất cả >>
-              </router-link>
-            </v-col>
-          </v-row>
+    <!-- Danh mục phim -->
+    <div v-for="(section, sectionIndex) in sections" :key="sectionIndex">
+      <v-row class="category-header" align="center" no-gutters>
+        <v-col cols="auto">
+          <h2 class="category-title">{{ section.title }}</h2>
+        </v-col>
+        <v-col cols="auto">
+          <router-link
+            :to="
+              section.name === 'QuocGia' || section.name === 'PhimNew'
+                ? {
+                    name: section.name,
+                    params: { path: section.id.split('/')[1] },
+                  }
+                : { name: section.name }
+            "
+            class="view-all"
+          >
+            Xem tất cả >>
+          </router-link>
+        </v-col>
+      </v-row>
 
-          <!-- Hiển thị phim dạng lưới -->
-          
-            <v-row no-gutters >
-              <v-col
-                v-for="(item, index) in isLoading ? Array(12).fill({}) : section.listMovie.slice(0, 12)"
-                :key="index"
-                cols="12"
-                sm="6"
-                md="4"
-                lg="2"
-                style="padding: 10px;"
-              >
-              <v-skeleton-loader
-                v-if="isLoading"
-                type="image"
-                height="250"
-              />
-              <router-link :to="{ name: 'MovieDetail', params: { slug: item.slug } }" v-else>
-                <v-card
-                  class="mx-auto bg-dark text-white"
-                  max-width="344"
-                >
-                <v-img
+      <!-- Hiển thị phim dạng lưới -->
+
+      <v-row no-gutters>
+        <v-col
+          v-for="(item, index) in isLoading
+            ? Array(12).fill({})
+            : section.listMovie.slice(0, 12)"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="2"
+          style="padding: 10px"
+        >
+          <v-skeleton-loader v-if="isLoading" type="image" height="250" />
+          <router-link
+            :to="{ name: 'MovieDetail', params: { slug: item.slug } }"
+            v-else
+          >
+            <v-card class="mx-auto bg-dark text-white" max-width="344">
+              <v-img
                 :src="getOptimizedImage(item.poster_url)"
                 :alt="`Poster phim ${item.name}`"
                 class="movie-img"
                 height="250"
                 cover
                 lazy-src
-                />
-                <v-card-subtitle style="color: #ffcc00;">
-                  {{item.episode_current === 'Tập 0' ? `Full-${item.lang}` : `${item.episode_current}-${item.lang}`}}
-                </v-card-subtitle>
-                <v-card-title class="movie-title">
-                  {{ item.name }}
-                </v-card-title>
-
-
-              </v-card>
-              </router-link>
-                
-              </v-col>
-            </v-row>
-         
-        </div>
-      </div>
+              >
+                <template #default>
+                  <v-btn
+                    icon
+                    size="small"
+                    color="red"
+                    variant="flat"
+                    class="favorite-btn"
+                    @click.stop="toggleFavorite(item)"
+                  >
+                    <v-icon>{{
+                      isFavorite(item) ? "mdi-heart" : "mdi-heart-outline"
+                    }}</v-icon>
+                  </v-btn>
+                </template>
+              </v-img>
+              <v-card-subtitle style="color: #ffcc00">
+                {{
+                  item.episode_current === "Tập 0"
+                    ? `Full-${item.lang}`
+                    : `${item.episode_current}-${item.lang}`
+                }}
+              </v-card-subtitle>
+              <v-card-title class="movie-title">
+                {{ item.name }}
+              </v-card-title>
+            </v-card>
+          </router-link>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
 </template>
-
-
-
 
 <script>
 import { ListMovieByCateHome, urlImage } from "@/model/api";
-import CarouselPage from './Carousel.vue';
+import CarouselPage from "./Carousel.vue";
 
 export default {
   name: "HomePage",
@@ -82,21 +96,52 @@ export default {
     return {
       urlImage: urlImage,
       isLoading: true,
+      favoriteMovies: [],
       sections: [
-        { title: this.$t('PHIM ĐỀ CỬ'), id: 'danh-sach/moi-nhat', name: 'PhimNew', listMovie: [] },
-        { title: this.$t('PHIM THỊNH HÀNH'), id: 'danh-sach/thinh-hanh', name: 'PhimNew', listMovie: [] },
-        { title: this.$t('PHIM BỘ'), id: 'danh-sach/phim-bo', name: 'PhimBo', listMovie: [] },
-        { title: this.$t('PHIM LẺ'), id: 'danh-sach/phim-le', name: 'PhimLe', listMovie: [] },
-        { title: this.$t('PHIM HÀN QUỐC'), id: 'quoc-gia/han-quoc', name: 'QuocGia', listMovie: [] },
-        { title: this.$t('PHIM TRUNG QUỐC'), id: 'quoc-gia/trung-quoc', name: 'QuocGia', listMovie: [] }
-      ]
+        {
+          title: this.$t("PHIM ĐỀ CỬ"),
+          id: "danh-sach/moi-nhat",
+          name: "PhimNew",
+          listMovie: [],
+        },
+        {
+          title: this.$t("PHIM THỊNH HÀNH"),
+          id: "danh-sach/thinh-hanh",
+          name: "PhimNew",
+          listMovie: [],
+        },
+        {
+          title: this.$t("PHIM BỘ"),
+          id: "danh-sach/phim-bo",
+          name: "PhimBo",
+          listMovie: [],
+        },
+        {
+          title: this.$t("PHIM LẺ"),
+          id: "danh-sach/phim-le",
+          name: "PhimLe",
+          listMovie: [],
+        },
+        {
+          title: this.$t("PHIM HÀN QUỐC"),
+          id: "quoc-gia/han-quoc",
+          name: "QuocGia",
+          listMovie: [],
+        },
+        {
+          title: this.$t("PHIM TRUNG QUỐC"),
+          id: "quoc-gia/trung-quoc",
+          name: "QuocGia",
+          listMovie: [],
+        },
+      ],
     };
   },
   components: {
-    CarouselPage
+    CarouselPage,
   },
   mounted() {
-    this.sections.forEach(item => {
+    this.sections.forEach((item) => {
       this.ListMovie(item.id, item);
     });
   },
@@ -105,7 +150,7 @@ export default {
       ListMovieByCateHome(
         sectionId,
         (result) => {
-          if (result.status === 'success') {
+          if (result.status === "success") {
             section.listMovie = result.data.items;
             this.isLoading = false;
           }
@@ -120,16 +165,28 @@ export default {
     },
     onImageLoad(index) {
       this.$nextTick(() => {
-        const imgRef = this.$refs['img_' + index];
+        const imgRef = this.$refs["img_" + index];
         if (imgRef && imgRef.$el) {
-          imgRef.$el.classList.add('loaded');
+          imgRef.$el.classList.add("loaded");
         }
       });
+    },
+    isFavorite(movie) {
+    return this.favoriteMovies.some(fav => fav.slug === movie.slug);
+  },
+
+  // Thêm/bỏ yêu thích
+  toggleFavorite(movie) {
+    const index = this.favoriteMovies.findIndex(fav => fav.slug === movie.slug);
+    if (index !== -1) {
+      this.favoriteMovies.splice(index, 1); // Bỏ yêu thích
+    } else {
+      this.favoriteMovies.push(movie); // Thêm yêu thích
     }
-  }
+  },
+  },
 };
 </script>
-
 
 <style scoped>
 .category-header {
@@ -161,11 +218,11 @@ export default {
   font-weight: 600;
   color: white;
   line-height: 1.2;
-  white-space: nowrap;             
-  overflow: hidden;                
-  text-overflow: ellipsis;       
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
-a{
+a {
   text-decoration: none;
   list-style: none;
 }
@@ -174,5 +231,11 @@ a{
   transition: transform 0.3s ease;
   box-shadow: 0 4px 20px rgba(255, 204, 0, 0.2);
 }
-
+.favorite-btn {
+  position: absolute;
+  top: 8px;
+  left: 8px;
+  z-index: 2;
+  background-color: rgba(0, 0, 0, 0.5);
+}
 </style>
