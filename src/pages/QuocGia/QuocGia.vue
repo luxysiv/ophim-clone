315272fc9bebc6d8495aa5c580e096b07b40537c
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-04-21 09:55:53 +0700

    Update QuocGia.vue

--- src/pages/QuocGia/QuocGia.vue
+++ src/pages/QuocGia/QuocGia.vue
@@ -115,7 +115,6 @@ export default {
 <style scoped>
 .movie-card {
   position: relative;
-  background-color: #1c1c1e;
   border-radius: 8px;
   overflow: hidden;
   transition: transform 0.2s;
@@ -143,7 +142,6 @@ export default {
 }
 
 .movie-title {
-  background: rgba(0, 0, 0, 0.7);
   color: white;
   position: absolute;
   bottom: 0;
