0620eb043aefc75506542e944bbd5d672a546640
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-04-21 09:57:28 +0700

    Update PhimBo.vue

--- src/pages/PhimBo/PhimBo.vue
+++ src/pages/PhimBo/PhimBo.vue
@@ -100,7 +100,6 @@ export default {
 <style scoped>
 .movie-card {
   position: relative;
-  background-color: #1c1c1e;
   border-radius: 8px;
   overflow: hidden;
   transition: transform 0.2s;
@@ -123,7 +122,6 @@ export default {
 }
 
 .movie-title {
-  background: rgba(0, 0, 0, 0.7);
   color: white;
   position: absolute;
   bottom: 0;
