824f64d3a0c4505bfcb312980a5802e6a1de3150
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-09-06 05:39:37 +0700

    kích hoạt trang Lịch sử

--- src/router/index.js
+++ src/router/index.js
@@ -1,106 +1,97 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routes =[
-    {
-        path: '/Error',
-        name: "ErrorPage",
-        component: () => import("@/pages/Error.vue")
-    },
-    {
-        
-        path: '',
-        name: "Layout",
-        redirect: '/home',
-        component: () => import("@/components/Layout"),
-        children: [
-        {
-            path: '/home',
-            name: "HomePage",
-            component: () => import("@/pages/Home")
-        },
-        {
-            path: '/movie/:slug',
-            name: "MovieDetail",
-            component: () => import("@/pages/MoiveDetail.vue"),
-            props: true
-        },
-        {
-            path: 'search',
-            name: "SearchMovie",
-            component: () => import("@/pages/SearchMovie.vue"),
-            props: true
-        },
-
-        {
-            path: '/phim-bo',
-            name: "PhimBo",
-            component: () => import("@/pages/PhimBo/PhimBo.vue")
-        },
-        {
-            path: '/phim-le',
-            name: "PhimLe",
-            component: () => import("@/pages/PhimLe/PhimLe.vue")
-        },
-        {
-            path: '/phim-sap-chieu',
-            name: "PhimSapChieu",
-            component: () => import("@/pages/PhimSapChieu/SapChieu.vue")
-        },
-        {
-            path: '/hoat-hinh',
-            name: "HoatHinh",
-            component: () => import("@/pages/HoatHinh/HoatHinh.vue")
-        },
-        {
-            path: '/the-loai/:path',
-            name: "TheLoai",
-            component: () => import("@/pages/TheLoai/TheLoai.vue"),
-            props: true
-        },
-        {
-            path: '/:path(phim-moi|phim-hot)',
-            name: "PhimNew",
-            component: () => import("@/pages/PhimNew/PhimNew.vue"),
-            props: true
-        },
-        {
-            path: '/quoc-gia/:path',
-            name: "QuocGia",
-            component: () => import("@/pages/QuocGia/QuocGia.vue"),
-            props: true
-        },
-        {
-            path: '/:catchAll(.*)',
-            redirect: '/Error'
-          }
-        
-        ]
-    }
-    
-
+const routes = [
+  {
+    path: '/Error',
+    name: "ErrorPage",
+    component: () => import("@/pages/Error.vue")
+  },
+  {
+    path: '',
+    name: "Layout",
+    redirect: '/home',
+    component: () => import("@/components/Layout"),
+    children: [
+      {
+        path: '/home',
+        name: "HomePage",
+        component: () => import("@/pages/Home")
+      },
+      {
+        path: '/movie/:slug',
+        name: "MovieDetail",
+        component: () => import("@/pages/MoiveDetail.vue"),
+        props: true
+      },
+      {
+        path: '/search',
+        name: "SearchMovie",
+        component: () => import("@/pages/SearchMovie.vue"),
+        props: true
+      },
+      {
+        path: '/phim-bo',
+        name: "PhimBo",
+        component: () => import("@/pages/PhimBo/PhimBo.vue")
+      },
+      {
+        path: '/phim-le',
+        name: "PhimLe",
+        component: () => import("@/pages/PhimLe/PhimLe.vue")
+      },
+      {
+        path: '/phim-sap-chieu',
+        name: "PhimSapChieu",
+        component: () => import("@/pages/PhimSapChieu/SapChieu.vue")
+      },
+      {
+        path: '/hoat-hinh',
+        name: "HoatHinh",
+        component: () => import("@/pages/HoatHinh/HoatHinh.vue")
+      },
+      {
+        path: '/the-loai/:path',
+        name: "TheLoai",
+        component: () => import("@/pages/TheLoai/TheLoai.vue"),
+        props: true
+      },
+      {
+        path: '/:path(phim-moi|phim-hot)',
+        name: "PhimNew",
+        component: () => import("@/pages/PhimNew/PhimNew.vue"),
+        props: true
+      },
+      {
+        path: '/quoc-gia/:path',
+        name: "QuocGia",
+        component: () => import("@/pages/QuocGia/QuocGia.vue"),
+        props: true
+      },
+      // Thêm route cho trang lịch sử
+      {
+        path: '/lich-su',
+        name: "HistoryPage",
+        component: () => import("@/pages/HistoryPage.vue")
+      },
+      {
+        path: '/:catchAll(.*)',
+        redirect: '/Error'
+      }
+    ]
+  }
 ]
 
-
-
-
-
-
-
-
-
-
-
 const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
-    routes
-  })
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
 
-  router.beforeEach((to, from, next) => {
-    if (to.matched.length === 0) {
-      next('/Error')
-    } else {
-      next()
-    }
-  })
+router.beforeEach((to, from, next) => {
+  if (to.matched.length === 0) {
+    next('/Error')
+  } else {
+    next()
+  }
+})
 
 export default router
